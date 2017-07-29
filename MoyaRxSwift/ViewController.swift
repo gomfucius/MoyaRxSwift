2dfe7fff7326755e9d0bf28ba47867ff3181c868
Genki Mine <gomfucius@gmail.com>
2017-07-28 21:52:45 -0700

    Single page app

---
+++ MoyaRxSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MoyaRxSwift
+//
+//  Created by Genki Mine on 7/28/17.
+//  Copyright © 2017 Genki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
